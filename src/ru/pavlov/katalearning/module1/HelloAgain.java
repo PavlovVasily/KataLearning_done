6be4916dc4846d01bed3d64bd4f0bfdf45f22897
PavlovVasiliy <vasylyjpavlov@mail.ru>
2021-12-23 09:55:15 +0300

    Initial commit

---
+++ src/ru/pavlov/katalearning/module1/HelloAgain.java
@@ -0,0 +1,7 @@
+package ru.pavlov.katalearning.module1;
+
+public class HelloAgain {
+    public static void main(String[] args) {
+        System.out.println("Hello world in thousand time");
+    }
+}
